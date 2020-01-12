9c006af22796b59fb54de841b55907943fa2c52c
alvaroximenesrj <alvaroximenesrj@gmail.com>
2020-01-12 17:03:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Moq Json Server
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9.
+
+## NPM Packages
+
+npm install --save-dev json-server@0.12.1</br>
+npm install --save-dev jsonwebtoken@8.1.1</br>
+json": "json-server data.js -p 3500 -m authMiddleware.js
+
+## Run moq json server
+
+npm run json</br>
+or</br>
+json-server --watch db.json
---
+++ data.js
@@ -0,0 +1,21 @@
+module.exports = function() {
+  return {
+    products: [
+      {
+        id: 1,
+        name: "Kayak",
+        category: "Watersports",
+        description: "A boat for one person",
+        price: 20
+      },
+      {
+        id: 2,
+        name: "Lifejacket",
+        category: "Watersports",
+        description: "Protective and fashionable",
+        price: 48.95
+      }
+    ],
+    orders: []
+  };
+};
